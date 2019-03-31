676e14068557e80f4308bacb5359f8b8f9e0364b
YUUKI Toriyama <43945424+YUUKIToriyama@users.noreply.github.com>
2019-03-31 10:11:03 +0900

    Update uncode.rb

--- uncode.rb
+++ uncode.rb
@@ -6,4 +6,10 @@
 unko = "0x1F4A9".hex.chr("UTF-8")
 unko_m = unko + "0xFE0E".hex.chr("UTF-8")
 
-puts gets.chomp.unpack("H*")[0].to_i(16).to_s(2).gsub("1", unko).gsub("0", unko_m)
+input_line = gets.chomp.split(" ")
+
+if (input_line[0] != "-d")
+	puts input_line[0].unpack("H*")[0].to_i(16).to_s(2).gsub("1", unko).gsub("0", unko_m)
+else
+	puts input_line[1].gsub(unko_m, "0").gsub(unko, "1").to_i(2).to_s(16).split("\n").pack("H*")
+end
